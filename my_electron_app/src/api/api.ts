10eacfb0d14bb60c873f14dd0f0eeb62a73c9f2f
zhangsiwen <13761394147@189.cn>
2023-08-17 15:26:28 +0800

    init

---
+++ my_electron_app/src/api/api.ts
@@ -0,0 +1,9 @@
+const baseUrl = import.meta.env.VITE_APP_PROXY_URL
+console.log(baseUrl)
+// export const getTabList = (params) => {
+//     return axios({
+//         method: 'post',
+//         url: baseUrl + 'QueryTabReq',
+//         data: params
+//     })
+// }
